5d00a55a24cc549bcdbe24793e47321aea9c5087
Steve Hartwell <steve.hartwell@gmail.com>
2025-02-05 21:43:51 -0800

    firefox, pwa userChrome restructure. (wip)

--- Firefox/Profiles/PWABaseProfile/user.js
+++
@@ -1,7 +0,0 @@
-//
-/******************************************************************************
- * user.js                                                                    *
- ******************************************************************************/
-
-user_pref('toolkit.legacyUserProfileCustomizations.stylesheets', true);
-// enabled to load "[user-profile]/chrome/user{Chrome,Content}.css"
