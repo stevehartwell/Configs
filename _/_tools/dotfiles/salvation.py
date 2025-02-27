ecf9226e74a069ad701cf99de1c1c4fff83af21a
Steve Hartwell <steve.hartwell@gmail.com>
2025-02-27 00:02:18 -0800

    zsh for fun and profit

--- _/_tools/dotfiles/salvation.py
+++ _/_tools/dotfiles/salvation.py
@@ -2,23 +2,24 @@
 
 # pylint: disable=invalid-name,missing-docstring
 
-'''
+"""
 Move files and replace with symlink
-'''
+"""
 
 import os
 from pathlib import Path
+
 from common import move_then_symlink
 
 
 def main(argv: list[str]):
-    save_dir = Path('/Volumes/NoSave/NAS/_Configs@/_')
+    save_dir = Path("/Volumes/Save/Users/sh/Configs/_")
     save_dir.mkdir(exist_ok=True)
     for item in [Path(i) for i in argv[1:] if not os.path.islink(i)]:
-        move_then_symlink(item, save_dir / item.name)
+        move_then_symlink(item=item, dest_path=save_dir / item.name)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
 
     main(sys.argv)
--- _/_tools/dotfiles/undotfiles.py
+++ _/_tools/dotfiles/undotfiles.py
@@ -2,42 +2,44 @@
 
 # pylint: disable=invalid-name,missing-docstring
 
-'''
+"""
 undotfiles
-'''
+"""
 
 from pathlib import Path
+
 from common import move_then_symlink
 
 
 def main():
-    dotless_dir = Path.home() / '_'
+    dotless_dir = Path.home() / "_"
     dotless_dir.mkdir(exist_ok=True)
-    for dotfile in [i for i in Path.home().iterdir() if i.name.startswith('.')]:
+    for dotfile in [i for i in Path.home().iterdir() if i.name.startswith(".")]:
         if (
-            dotfile.name.endswith('_history')
+            dotfile.name.endswith("_history")
             or dotfile.name in exceptions
             or dotfile.is_symlink()
         ):
             continue
         dotless_path = dotless_dir / dotfile.name[1:]
-        move_then_symlink(dotfile, dotless_path)
+        print(f"{dotfile} -> {dotless_path}")
+        move_then_symlink(item=dotfile, dest_path=dotless_path)
 
 
 exceptions = set(
     [
-        '.CFUserTextEncoding',
-        '.cups',
-        '.DS_Store',
-        '.lesshst',
-        '.ssh',
-        '.stCommitMsg',
-        '.Trash',
-        '.viminfo',
-        '.zsh_sessions',
+        ".CFUserTextEncoding",
+        ".cups",
+        ".DS_Store",
+        ".lesshst",
+        ".ssh",
+        ".stCommitMsg",
+        ".Trash",
+        ".viminfo",
+        ".zsh_sessions",
     ]
 )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
